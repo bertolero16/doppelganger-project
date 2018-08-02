841c90a1df206e4c535d492a0505acd5b8e45d65
Andrew Bertolero <Bert@Andrews-MacBook-Air-2.local>
2018-08-02 15:32:49 -0700

    fixed upload file code; saves to uploads folder

--- src/flask_server.py
+++ src/flask_server.py
@@ -4,8 +4,8 @@ import pandas as pd
 import time
 import json
 from pandas.io.json import json_normalize
-from flask_pymongo import PyMongo
 
+from templates import *
 import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
@@ -15,13 +15,17 @@ app = Flask(__name__)
 
 ###########################
 
+
+
+
 # Uploading Files:
 
-UPLOAD_FOLDER = '/path/to/the/uploads' # <---- set path to uploads folder
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg')
+UPLOAD_FOLDER = './uploads' # <---- set path to uploads folder
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -43,88 +47,104 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
+            return redirect(url_for('upload_file',
                                     filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    return render_template('upload_page.html')
 
-#######################
+# Homepage 
+
+@app.route('/homepage')
+def homepage():
+    # return 'hey buddddy'
+    return render_template('homepage.html')
 
 
 
-with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
-
-@app.route('/', methods = ['GET'])
-def something():
-    get_data()
-    df = pd.read_json('newdata.json')
-    df = json_normalize(list(df.data))
-    mindf = feature_engineer(df)
-    y_hat = model.predict_proba(mindf)
-    with open ('prediction.csv','w') as f:
-        f.write(str(y_hat[0][1]))
-
-    prob = pd.read_csv('prediction.csv',header=None,names=['probability'])
-    p = prob.probability
-    df['probability'] = p
-
-    if p[0] > 0.8:
-        df['label'] = 'high risk'
-    elif p[0] <= 0.8 and p[0] >0.6:
-        df['label'] = 'middle risk'
-    elif p[0] <= 0.6 and p[0] >=0.5:
-        df['label'] = 'low risk'
-    # if 'label' in df.keys():
-    else:
-        df['label'] = 'Not Fraud'
-    df.to_json('df_predictions.json')
-    import os
-    os.system("mongoimport -h ds155461.mlab.com:55461-d fraud_db -c events -u bertolero16 -p lambo5 --file df_predictions.json")
-
-# Route for getting new data
-def get_data():
-    import requests
-    api_key = 'vYm9mTUuspeyAWH1v-acfoTlck-tCxwTw9YfCynC'
-    url = 'https://hxobin8em5.execute-api.us-west-2.amazonaws.com/api/'
-    sequence_number = 0
-    response = requests.post(url, json={'api_key': api_key,
-                                        'sequence_number': sequence_number})
-    raw_data = response.json()
-    with open ('newdata.json','w') as f:
-        f.write(json.dumps(raw_data))
-
-    # flask pymongo
-    # app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
-    # mongo = PyMongo(app)
-    # doc = mongo.db.fraud.insert(raw_data)
-
-
-
-    #mongo db
-    # from bson import json_util
-    # data = json_util.loads(response.json())
-    # # data = json.loads(raw_data) #json.loads returns dictionary
-    # from pymongo import MongoClient
-    # mongo_client = MongoClient()
-    # db = mongo_client.mydb2
-    # coll = db.fraud
-    # # posts = db.posts
-    # # #mongo needs to insert dictionary, however not a json file 
-    # # post_id = posts.insert_one(raw_data['data']).inserted_ids
-    # coll.insert_one(data)
-
-
-    return jsonify(raw_data)
-
+##################################
 
 
 if __name__ == '__main__':
     app.run(host ='0.0.0.0', port = 3333, debug = True)
+
+
+
+###################################
+
+
+
+
+
+
+
+# # Previous Fraud website:
+
+# with open('model.pkl', 'rb') as f:
+#     model = pickle.load(f)
+
+# @app.route('/', methods = ['GET'])
+# def something():
+#     get_data()
+#     df = pd.read_json('newdata.json')
+#     df = json_normalize(list(df.data))
+#     mindf = feature_engineer(df)
+#     y_hat = model.predict_proba(mindf)
+#     with open ('prediction.csv','w') as f:
+#         f.write(str(y_hat[0][1]))
+
+#     prob = pd.read_csv('prediction.csv',header=None,names=['probability'])
+#     p = prob.probability
+#     df['probability'] = p
+
+#     if p[0] > 0.8:
+#         df['label'] = 'high risk'
+#     elif p[0] <= 0.8 and p[0] >0.6:
+#         df['label'] = 'middle risk'
+#     elif p[0] <= 0.6 and p[0] >=0.5:
+#         df['label'] = 'low risk'
+#     # if 'label' in df.keys():
+#     else:
+#         df['label'] = 'Not Fraud'
+#     df.to_json('df_predictions.json')
+#     import os
+#     os.system("mongoimport -h ds155461.mlab.com:55461-d fraud_db -c events -u bertolero16 -p lambo5 --file df_predictions.json")
+
+# # Route for getting new data
+# def get_data():
+#     import requests
+#     api_key = 'vYm9mTUuspeyAWH1v-acfoTlck-tCxwTw9YfCynC'
+#     url = 'https://hxobin8em5.execute-api.us-west-2.amazonaws.com/api/'
+#     sequence_number = 0
+#     response = requests.post(url, json={'api_key': api_key,
+#                                         'sequence_number': sequence_number})
+#     raw_data = response.json()
+#     with open ('newdata.json','w') as f:
+#         f.write(json.dumps(raw_data))
+
+# # didn't use code below:
+
+#     # flask pymongo
+#     # app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+#     # mongo = PyMongo(app)
+#     # doc = mongo.db.fraud.insert(raw_data)
+
+
+
+#     #mongo db
+#     # from bson import json_util
+#     # data = json_util.loads(response.json())
+#     # # data = json.loads(raw_data) #json.loads returns dictionary
+#     # from pymongo import MongoClient
+#     # mongo_client = MongoClient()
+#     # db = mongo_client.mydb2
+#     # coll = db.fraud
+#     # # posts = db.posts
+#     # # #mongo needs to insert dictionary, however not a json file 
+#     # # post_id = posts.insert_one(raw_data['data']).inserted_ids
+#     # coll.insert_one(data)
+
+
+#     return jsonify(raw_data)
+
+
+
+
